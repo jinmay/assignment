49288460d0b14f6c58f00e79be33e6c3290bf115
jinmay <britzegs@gmail.com>
2022-05-10 14:32:40 +0900

    Remove login_required decorator

--- forums/views.py
+++ forums/views.py
@@ -1,6 +1,3 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-
 from rest_framework.generics import ListCreateAPIView
 
 from .models import (
@@ -11,7 +8,6 @@ from .serializers import (
 )
 
 
-@method_decorator(login_required, name='post')
 class QuestionListCreateView(ListCreateAPIView):
     serializer_class = QuestionSerializer
     queryset = Question.objects.all()
