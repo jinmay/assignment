757534dc76bdaf959f8238dec952596fd1b37da7
jinmay <britzegs@gmail.com>
2022-05-10 11:02:54 +0900

    Init project

---
+++ modulab/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for modulab project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modulab.settings')
+
+application = get_wsgi_application()
