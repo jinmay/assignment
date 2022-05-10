b93b00e96d87776ea3d70ff853c59dc83f2e724f
jinmay <britzegs@gmail.com>
2022-05-10 14:14:27 +0900

    Add Question model class

--- forums/models.py
+++ forums/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=128)
+    body = models.TextField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
