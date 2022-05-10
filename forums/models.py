a6111e947f095e8643add751f1ec000b074ec3fe
jinmay <britzegs@gmail.com>
2022-05-10 22:29:58 +0900

    Add Comment model

--- forums/models.py
+++ forums/models.py
@@ -14,3 +14,12 @@ class Question(models.Model):
     def check_permission(self, user):
         if user.id != self.owner_id:
             raise PermissionDenied()
+
+
+class Comment(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    body = models.TextField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
