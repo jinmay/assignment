04a83834bb6fefcfba2b057e180c5d59c29ab785
jinmay <britzegs@gmail.com>
2022-05-10 15:30:32 +0900

    Add method for check permission of question

--- forums/models.py
+++ forums/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
 
+from .exceptions import PermissionDenied
+
 
 class Question(models.Model):
     title = models.CharField(max_length=128)
@@ -8,3 +10,7 @@ class Question(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def check_permission(self, user):
+        if user.id != self.owner_id:
+            raise PermissionDenied()
